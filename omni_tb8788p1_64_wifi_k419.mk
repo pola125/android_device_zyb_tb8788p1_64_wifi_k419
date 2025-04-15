bb73d5f1208216b0bc2d2681eb0628186e09a789
pola125 <3597599759@qq.com>
2025-04-15 21:30:29 +0800

    Update omni_tb8788p1_64_wifi_k419.mk

--- omni_tb8788p1_64_wifi_k419.mk
+++ omni_tb8788p1_64_wifi_k419.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from tb8788p1_64_wifi_k419 device
 $(call inherit-product, device/zyb/tb8788p1_64_wifi_k419/device.mk)
